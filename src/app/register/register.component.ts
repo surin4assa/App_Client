712398ea3d76ac42c660d76c0aced9c9425a0003
SurinA <13841818+surin4assa@users.noreply.github.com>
2021-10-08 17:26:38 -0500

    RouteGaurd + bootstrap theme

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -18,7 +19,15 @@ export class RegisterComponent implements OnInit {
     this.accountService.register(this.model).subscribe(response => {
       console.log(response)
       this.cancel();
-    }, error => console.error(error))
+    }, error => {
+      //console.error(error);
+      if(error.error.errors){
+          for (let e in error.error.errors){
+            this.toastr.error(error.error.errors[e]);
+          }
+      }
+      else this.toastr.error(error.error);
+    })
   }
 
   cancel(){
