04d196bb6bf05da3d6ce3bce7df94d19a53868b4
SurinA <13841818+surin4assa@users.noreply.github.com>
2021-10-11 21:25:57 -0500

    end of section 13

--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,11 @@
+import { User } from 'src/app/_models/user';
+import { AccountService } from './../../_services/account.service';
+import { UserParams } from './../../_models/userParams';
+import { Pagination } from './../../_models/pagination';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-list',
@@ -9,13 +13,38 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  userParams: UserParams;
+  user: User;
+  genderList = [{value: 'male', display: 'Male'}, {value: 'female', display: 'Female'}];
 
-  constructor(private memberService: MembersService) { }
+  constructor(private memberService: MembersService) {
+    this.userParams = this.memberService.getUserParams();
+  }
 
   ngOnInit(): void {
-    //this.loadMembers();
-    this.members$ = this.memberService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers(){
+    //console.log(this.userParams)
+    this.memberService.setUserparams(this.userParams);
+    this.memberService.getMembers(this.userParams).subscribe(response => {
+      this.members = response.result;
+      this.pagination = response.pagination;
+    })
+  }
+
+  resetFilters(){
+    this.userParams = this.memberService.resetUserParams();
+    this.loadMembers();
+  }
+
+  pageChanged(event: any){
+    this.userParams.pageNumber = event.page;
+    this.memberService.setUserparams(this.userParams)
+    this.loadMembers();
   }
 
 }
